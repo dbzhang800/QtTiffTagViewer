0d5276ee2992ced46d6ddb1bcb148428e87fe48f
Debao Zhang <hello@debao.me>
2018-08-24 14:50:50 +0800

    Fix license header in optionsdialog
    Change-Id: If3286c087b8d179f90ad00ea3d97aecb3a1f3361

--- src/optionsdialog.h
+++ src/optionsdialog.h
@@ -1,14 +1,27 @@
-/**************************************************************************
+/****************************************************************************
+** Copyright (c) 2018 Debao Zhang <hello@debao.me>
+** All right reserved.
 **
-** Copyright (C) 2018 Focus e-Beam Technology (Beijing) Co.,Ltd.
-** Contact: contact@focus-ebeam.com
+** Permission is hereby granted, free of charge, to any person obtaining
+** a copy of this software and associated documentation files (the
+** "Software"), to deal in the Software without restriction, including
+** without limitation the rights to use, copy, modify, merge, publish,
+** distribute, sublicense, and/or sell copies of the Software, and to
+** permit persons to whom the Software is furnished to do so, subject to
+** the following conditions:
 **
-** This file is part of <......>.
+** The above copyright notice and this permission notice shall be
+** included in all copies or substantial portions of the Software.
 **
-** This source code is confidential information and can be used only as
-** authorized by a licensing agreement from FBT.
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+** NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+** LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+** OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+** WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **
-**************************************************************************/
+****************************************************************************/
 #ifndef OPTIONSDIALOG_H
 #define OPTIONSDIALOG_H
 
